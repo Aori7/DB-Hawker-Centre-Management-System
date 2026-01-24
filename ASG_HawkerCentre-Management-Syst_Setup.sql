80ff42c67878c77e1e3801aa851571751e0a5ecc
Adawiyah <n.adawiyahisham@gmail.com>
2026-01-24 15:21:24 +0800

    created operator table

--- ASG_HawkerCentre-Management-Syst_Setup.sql
+++ ASG_HawkerCentre-Management-Syst_Setup.sql
@@ -4,9 +4,33 @@
 /* Group 4*/
 /* Created by Ada */
 
+-- NOTE to teamates: run once!
+
 /* creating new database */
+
+/*
 Create Database HCManagementSystem;
 GO
+*/
+
+-- create tables according to data dictionary
+
+/*Format:
+CREATE TABLE table_name
+(column_name  format [column_constraint]
+ {,column_name  format [column_constraint]}
+ [,table_constraint {,table_constraint}]
+); */
+
+CREATE TABLE Operator
+(
+OperatorID		varchar(7) NOT NULL,
+OperatorName	char(50)	NOT NULL,
+ContactPerson	char(50)	NOT NULL,
+	CONSTRAINT PK_Operator PRIMARY KEY (OperatorID)
+);
+GO
+
 
 
 
